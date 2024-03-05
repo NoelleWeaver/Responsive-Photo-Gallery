cea14be9534b71b022da8c3c56e99e4d6ab65e94
NoelleWeaver <141285591+NoelleWeaver@users.noreply.github.com>
2024-03-04 18:17:58 -0700

    a

--- ahh.js
+++ ahh.js
@@ -1,12 +1,12 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+// showSlides(slideIndex);
 
 function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
 
 function closeModal() {
-    document.getElementById("myModal").style.display = "none";
+    document.getElementByClassName("fullscreen")[0].style.display = "none";
 }
 
 function plusSlides(n) {
@@ -16,14 +16,23 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+function imageLoader(location){
+    //div background to image url
+    document.getElementsByClassName('fullscreen')[0].style.backgroundImage = `url("${location}")`
+    //set display to block 
+    console.log('test')
+    document.getElementsByClassName('fullscreen')[0].style.display = 'block';
+    document.getElementByClassName('fullscreen')[0].style.backgroundSize = 'cover';
+    document.getElementByClassName('fullscreen')[0].style.backgroundSize = 'cover';
 }
+
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[slideIndex-1].style.display = "block";
+// }
